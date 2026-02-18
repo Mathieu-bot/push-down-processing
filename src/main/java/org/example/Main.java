0fca28316c0c7433e427de1c0e09def73ffd9a93
Mathieu <hei.tafita.2@gmail.com>
2026-02-18 16:31:15 +0300

    add somme test in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.example.service.DataRetriever;
+import org.example.database.DBConnection;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+
+        DBConnection dbConnection = new DBConnection();
+        DataRetriever service = new DataRetriever(dbConnection);
+
+        System.out.println("=== Q1 - Total par facture ===");
+        service.findInvoiceTotals().forEach(System.out::println);
+
+        System.out.println("\n=== Q2 - Factures CONFIRMED et PAID ===");
+        service.findConfirmedAndPaidInvoiceTotals().forEach(System.out::println);
+
+        System.out.println("\n=== Q3 - Totaux cumulés par statut ===");
+        System.out.println(service.computeStatusTotals());
+
+        System.out.println("\n=== Q4 - Chiffre d’affaires pondéré HT ===");
+        System.out.println(service.computeWeightedTurnover());
+
+        System.out.println("\n=== Q5-A - Totaux HT, TVA et TTC ===");
+        service.findInvoiceTaxSummaries().forEach(System.out::println);
+
+        System.out.println("\n=== Q5-B - Chiffre d’affaires TTC pondéré ===");
+        System.out.println(service.computeWeightedTurnoverTtc());
     }
 }
